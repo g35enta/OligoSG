491df7325f2a07ab899caac5c94d70cee9cc6b86
g35enta <genta.ito@gmail.com>
2020-12-28 10:50:31 +0900

    プロジェクト ファイルを追加します。

---
+++ OligoSG/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OligoSG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
